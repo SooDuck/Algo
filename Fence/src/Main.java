1e32c332d874d83caf9145332cbbc0d4fc14cf30
Sooduck <psd634@naver.com>
2018-03-30 20:29:31 +0900

    fence problem dealwith way on Book

--- Fence/src/Main.java
+++ Fence/src/Main.java
@@ -2,87 +2,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    static final int HEIHSTNUM = 987654321;
-    static int[] fence(List<Integer> arr, int deep) {
+    static List<Integer> input = new ArrayList<>();
 
-        int size = arr.size();
+    static int fence(int left, int right) {
 
-        int[] left = null;
-        int[] right = null;
+        if(left == right) return input.get(left);
 
-        int highestFence = 0;
-        int cnsctNmbr = 1;
+        int mid = (left + right) / 2;
 
-        // 작은 단위로 분해, 이분화된 문제중 답에 근접한 값 저장
-        if(size > 1) {
-            left = fence(arr.subList(0, size/2),deep+1);
-            right = fence(arr.subList(size/2, size),deep+1);
+        int ret = Math.max(fence(left, mid), fence(mid+1, right));
 
-            if ((left[0]*left[1]) > (right[0]*right[1])) {
-                highestFence = left[0];
-                cnsctNmbr = left[1];
-            } else if((left[0]*left[1]) < (right[0]*right[1])) {
-                highestFence = right[0];
-                cnsctNmbr = right[1];
+        int lo = mid, hi = mid+1;
+
+        int height = Math.min(input.get(lo), input.get(hi));
+        ret = Math.max(ret, height*2);
+
+        while (left < lo || hi < right) {
+            if(hi < right && (lo == left || input.get(lo-1) < input.get(hi+1))) {
+                ++hi;
+                height = Math.min(height, input.get(hi));
+            } else {
+                --lo;
+                height = Math.min(height, input.get(lo));
             }
+
+            ret = Math.max(ret, height * (hi - lo  + 1));
         }
 
-        System.out.println("split done");
+        return ret;
 
-
-        // 분해된 문제에 대한 처리
-        // 기존의 가로 * 세로의 크기와
-        // 현재 배열의 가장 높은 울타리부터 가장 낮은 울타리까지, 연속수 증가
-        // 2 3 4
-        // 6 2 3
-        // 4 3 2
-
-
-        int fence = arr.get(0);
-        int low = 0;
-
-        int resultFence = arr.get(0);
-        int resultLow = 1;
-
-        System.out.println("in for");
-        for(int i=size; i>0; i--) {
-            // 비교처리
-
-        }
-
-        // 9 >= 6 f6 l2 r12
-        // 6 >= 7
-
-        // 2 >= 3 2 1
-
-        // 2 > 3 || 2 < 6
-
-        // 제일 큰걸찾고 기존에서 넘겨받은 결과와 비교
-        int[] result = new int[2];
-
-        result[0] = resultFence;
-        result[1] = resultLow;
-
-        if((highestFence * cnsctNmbr) > (resultFence * resultLow) ) {
-            result[0] = highestFence;
-            result[1] = cnsctNmbr;
-        }
-
-        for(int i=0; i < deep; i++) {
-            System.out.print("   ");
-        }
-        System.out.println(Arrays.toString(arr.toArray()) + " fence : " + result[0] + " cnsct : " + result[1] + " largest : " + (result[0] * result[1]));
-
-
-//        int result = highestFence * cnsctNmbr;
-
-        return result; //most lowest num;
     }
 
 
@@ -96,9 +51,9 @@ public class Main {
 
         for(int i=0; i<n; i++) {
             String low = br.readLine();
-            List<Integer> input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).boxed().collect(Collectors.toList());
-            int[] result = fence(input,0);
-            System.out.println(result[0] * result[1]);
+            input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).boxed().collect(Collectors.toList());
+            int result = fence(0,input.size()-1);
+            System.out.println(result);
         }
     }
 }
