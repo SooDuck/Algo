0a8aa6a9e60a6fb96bbc2b4cf1901cad65f57170
Sooduck <psd634@naver.com>
2019-05-19 14:18:32 +0900

    Change way, SEPARATE to GROUPING

--- FlattenLand/src/Main.java
+++ FlattenLand/src/Main.java
@@ -4,14 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        long result;
+
         System.out.println("Hello World!");
 
 //        long result = new Solution().solution(new int[][]{{1, 2}
 //                                                           , {2, 3}}, 3, 2);
 
-        long result = new Solution().solution(new int[][]{{4, 4, 3}
-                                                          , {3, 2, 2}
-                                                          , {2, 1, 0}}, 5, 3);
+//        long result = new Solution().solution(new int[][]{{4, 4, 3}
+//                                                          , {3, 2, 2}
+//                                                          , {2, 1, 0}}, 5, 3);
 
 
 //        long result = new Solution().solution(new int[][]{{4, 4, 3, 2}
@@ -20,7 +22,7 @@ public class Main {
 //                                                        , {6, 3, 4 ,1}}, 2, 3);
 
 
-//        long result = new Solution().solution(new int[][]{{0, 0, 0, 0}
+//        result = new Solution().solution(new int[][]{{0, 0, 0, 0}
 //                , {0, 0, 0, 0}
 //                , {0, 0, 0, 0}
 //                , {6, 0, 0, 0}}, 2, 103);
@@ -41,6 +43,32 @@ public class Main {
 //                , {0, 5, 3, 5}
 //                , {5, 4, 2, 3}}, 2, 103);
 
-        System.out.println("result : " + result);
+
+
+
+        result = new Solution().solution(new int[][]{{0, 1, 3, 2}
+                , {2, 0, 2, 0}
+                , {9, 1, 0, 3}
+                , {7, 2, 3 ,1}}, 2, 3);
+
+        System.out.println("Solution result : " + result);
+
+
+//        long result = new Sol().solution(new int[][]{{4, 4, 3}
+//                , {3, 2, 2}
+//                , {2, 1, 0}}, 5, 3);
+
+
+        System.out.println("--------------------------------------------");
+//        result = new Sol().solution(new int[][]{{0, 0, 0, 0}
+//                , {0, 0, 0, 0}
+//                , {0, 0, 0, 0}
+//                , {6, 0, 0, 0}}, 2, 103);
+
+        result = new Sol().solution(new int[][]{{0, 1, 3, 2}
+                                                        , {2, 0, 2, 0}
+                                                        , {9, 1, 0, 3}
+                                                        , {7, 2, 3 ,1}}, 2, 3);
+        System.out.println("Sol result : " + result);
     }
 }
--- FlattenLand/src/Solution.java
+++ FlattenLand/src/Solution.java
@@ -62,6 +62,9 @@ public class Solution {
 
         long max = Arrays.stream(land).flatMapToLong(array -> Arrays.stream(array).asLongStream()).summaryStatistics().getMax();
 
+//        long[] temp = Arrays.stream(land).flatMapToLong(array -> Arrays.stream(array).asLongStream()).toArray();
+//        Arrays.parallelSort(temp);
+//        long max = temp[temp.length-1];
 
 //        long bf = Long.MAX_VALUE;
 
@@ -75,62 +78,62 @@ public class Solution {
 
         long first = 0;
         long last = max;
-        long left;
-        long right;
 
-        long[] cache = new long[(int) max + 1];
-        Arrays.fill(cache, -1);
-
-        long firstVal = Long.MAX_VALUE;
+//        long firstVal = Long.MAX_VALUE;
 //        long midVal = Long.MAX_VALUE;
+
+        Map<Long, Long> cache = new HashMap<>();
 
         while (first <= last) {
 
+            if(!cache.containsKey(first)) cache.put(first, calc(land, first, P, Q));
+            if(!cache.containsKey(last)) cache.put(last, calc(land, last, P, Q));
+
             if((last - first) < 3) {
-                firstVal = calc(land, first, P, Q);
-                long midVal = calc(land, (last-first)+1, P, Q);
-                long lastVal = calc(land, last, P, Q);
 
-//                System.out.println(first + " - " + ((last-first)+1) + " - " + last);
-//                System.out.println(firstVal + " - " + midVal + " - " + lastVal);
+                long mid = (last - first) + 1;
 
-                return Math.min(firstVal, Math.min(lastVal, midVal));
+                if(!cache.containsKey(mid)) cache.put(mid, calc(land, mid, P, Q));
+
+                System.out.println(first + " - " + ((last-first)+1) + " - " + last);
+                System.out.println(cache.get(first) + " - " + cache.get(mid) + " - " + cache.get(last));
+
+                return Math.min(cache.get(first), Math.min(cache.get(last), cache.get(mid)));
             }
 
-            left = (long) (Math.ceil((last - first) / 3) * 1);
-            right = (long) (Math.ceil((last - first) / 3) * 2);
+            long left = (long) (Math.ceil((last - first) / 3) * 1);
+            long right = (long) (Math.ceil((last - first) / 3) * 2);
 
-//            System.out.println(first + " - " + left + " - " + right + " - " + last);
+            System.out.println(first + " - " + left + " - " + right + " - " + last);
 
-//            midVal = calc(land, mid, P, Q);
-//            firstVal = calc(land, first, P, Q);
-//            long leftVal = calc(land, left, P, Q);
-//            long rightVal = calc(land, right, P, Q);
-//            long lastVal = calc(land, last, P, Q);
+            if(!cache.containsKey(left)) cache.put(left, calc(land, left, P, Q));
+            if(!cache.containsKey(right)) cache.put(right, calc(land, right, P, Q));
 
-
-            firstVal = (cache[(int) first] != -1) ? cache[(int) first] : calc(land, first, P, Q);
-            long leftVal = (cache[(int) left] != -1) ? cache[(int) left] : calc(land, left, P, Q);
-            long rightVal = (cache[(int) right] != -1) ? cache[(int) right] : calc(land, right, P, Q);
-            long lastVal = (cache[(int) last] != -1) ? cache[(int) last] : calc(land, last, P, Q);
-
-            if(cache[(int) first] != -1) cache[(int) first] = firstVal;
-            if(cache[(int) left] != -1) cache[(int) left] = leftVal;
-            if(cache[(int) right] != -1) cache[(int) right] = rightVal;
-            if(cache[(int) last] != -1) cache[(int) last] = lastVal;
+//            long firstVal = cache.get(first);
+//            long leftVal = cache.get(left);
+//            long rightVal = cache.get(right);
+//            long lastVal = cache.get(last);
 
 //            System.out.println("------------------------");
-//            System.out.println(first + " " + firstVal);
-//            System.out.println(left + " " + leftVal);
-//            System.out.println(right + " " + rightVal);
-//            System.out.println(last + " " + lastVal);
+//            System.out.println(first + " " + cache.get(first));
+//            System.out.println(left + " " + cache.get(left));
+//            System.out.println(right + " " + cache.get(right));
+//            System.out.println(last + " " + cache.get(last));
 //            System.out.println("------------------------");
 
             Map<Long, Long> lowestMap = new HashMap<>();
-            lowestMap.put(first, firstVal);
-            lowestMap.put(left, leftVal);
-            lowestMap.put(right, rightVal);
-            lowestMap.put(last, lastVal);
+            lowestMap.put(first, cache.get(first));
+            lowestMap.put(left, cache.get(left));
+            lowestMap.put(right, cache.get(right));
+            lowestMap.put(last, cache.get(last));
+
+//            long[][] compa = new long[4][4];
+//
+//            compa[0][0] = first; compa[0][1] = cache.get(first);
+//            compa[1][0] = left;  compa[1][1] = cache.get(left);
+//            compa[2][0] = right; compa[2][1] = cache.get(right);
+//            compa[3][0] = last;  compa[3][1] = cache.get(last);
+
 
             List list = sortByValue(lowestMap);
 
@@ -142,7 +145,7 @@ public class Solution {
 
         }
 
-            answer = firstVal;
+            answer = cache.get(first);
 
         return answer;
     }
@@ -150,31 +153,19 @@ public class Solution {
     private static List sortByValue(Map map) {
 
         List<Long> list = new ArrayList<>();
-
-
         list.addAll(map.keySet());
 
-//        Collections.sort(list, new Comparator() {
-//            @Override
-//            public int compare(Object o1, Object o2) {
-//                return ((Comparable) map.get(o2)).compareTo(map.get(o1));
-//            }
+//        list.forEach(i -> {
+//            System.out.println(i + " - " + map.get(i));
 //        });
 
-        Collections.sort(list, (o1, o2) -> ( -1 * ((Comparable) map.get(o2)).compareTo(map.get(o1))));
+        Collections.sort(list, (o1, o2) -> {
+
+//            System.out.println(map.get(o2) + " - " + map.get(o1));
+
+            return ( -1 * ((Comparable) map.get(o2)).compareTo(map.get(o1)));
+        });
 //        Collections.reverse(list);
-
-//        System.out.println("*****************************************");
-//        for (Object a : map.entrySet()) {
-//            String[] b = String.valueOf(a).split("=");
-//            System.out.println(b[0] + " - " + b[1]);
-//        }
-//
-//        for (long a : list) {
-//            System.out.println(a);
-//        }
-//        System.out.println("*****************************************");
-
 
         return list;
     }
